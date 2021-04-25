ac55a558bed64be234c9205d8ea5950a901c883b
Jerry Xiong <xion0597@umn.edu>
2021-04-25 16:17:12 -0500

    completed stretch goal 8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -100,8 +100,12 @@ console.log('running find', find('d', alphabet) );
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string){
-
-}
+  if ( letter === string.slice(0,1) ){
+      return true;
+  } else {
+      return false;
+  }
+} // end isFirstLetter
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
